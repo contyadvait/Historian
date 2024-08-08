ab74fcae6af650d66b78849f820b0b2fc45b8f8e
contyadvait <advaitthepro@outlook.com>
2024-08-08 19:13:28 +0800

    Initial Commit

---
+++ Historian/HistorianApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistorianApp.swift
+//  Historian
+//
+//  Created by Milind Contractor on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HistorianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
